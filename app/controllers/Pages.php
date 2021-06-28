8ec53cc14b2599f656d40b39e81cc57f4a2c9742
Loósz Roland Gábor <h984129@stud.u-szeged.hu>
2021-06-28 18:07:03 +0200

    Init

---
+++ app/controllers/Pages.php
@@ -0,0 +1,14 @@
+<?php
+class Pages {
+	public function __construct() {
+		echo "Loaded";
+	}
+
+	public function index() {
+		echo 'Home page';
+	}
+
+	public function about() {
+		echo 'About';
+	}
+}
