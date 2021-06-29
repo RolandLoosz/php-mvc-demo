9ad2bf6501321fa837cc5dafbe58025cbe81feed
Loósz Roland Gábor <h984129@stud.u-szeged.hu>
2021-06-29 14:18:51 +0200

    coloring

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,6 +1,6 @@
 <?php 
-
-require APPROOT . '/views/includes/head.php';
+// todo . check later
+// require APPROOT . '/views/includes/head.php';
 /*
 foreach ($data['users'] as $user) {
 	echo "Information: " . $user->name;
@@ -8,5 +8,83 @@ foreach ($data['users'] as $user) {
 }
 */
 
+// <?php echo URLROOT;
+
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php echo SITENAME; ?></title>
+	<!-- todo . remove -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+
+<style>
+
+header {
+    text-align: center;
+}
+
+main {
+    display: flex;
+    justify-content: space-around;
+}
+
+
+main > a {
+	border-radius: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    min-height: 250px;
+    margin: 10px;
+    border: 3px solid #333;
+    color: #333333;
+	background: #00BFFF;
+}
+
+main > a > h3 {
+    font-size: 28px;
+}
+
+a:link,
+a:visited
+{
+    text-decoration: none;
+}
+
+a:hover {
+    background-color: #17c5ff;
+}
+
+a:active
+{
+    text-decoration: none;
+}
+
+body {
+	background: linear-gradient(90deg, #00d2ff 0%, #3a47d5 100%);
+}
+
+</style>
+
+
+</head>
+<body>
+
+	<header>
+		<h2>Advertisement Page</h2>
+	</header>
+	<main>
+		<button type="submit" formmethod="get">Users</button>
+		<a href="<?php echo URLROOT; ?>/pages/users"><h3>Users</h3></a>
+		<a href="<?php echo URLROOT; ?>/pages/advertisement"><h3>Advertisement</h3></a>
+		<!-- <a href="?page=ads"><h3>Advertisements</h3></a> -->
+	</main>
+
+</body>
+</html>
